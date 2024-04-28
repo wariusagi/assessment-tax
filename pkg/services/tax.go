bdfda80109ccbdae7592fa2f9b42e89410833958
wariusagi <54016889+wariusagi@users.noreply.github.com>
2024-04-28 19:59:25 +0700

    feature/story-4 (#4)
    * add response field taxLevel
    
    * add unit test of response taxLevel

--- pkg/services/tax.go
+++ pkg/services/tax.go
@@ -14,21 +14,28 @@ type TaxCalculationRequest struct {
 }
 
 type TaxCalculationResponse struct {
-	Tax float64 `json:"tax"`
+	Tax      float64       `json:"tax"`
+	TaxLevel []TaxLevelRes `json:"taxLevel"`
 }
 
-type TaxLevel struct {
+type TaxLevelRes struct {
+	Level string  `json:"level"`
+	Tax   float64 `json:"tax"`
+}
+
+type TaxCalculatorLevel struct {
 	Lower float64
 	Upper float64
 	Rate  float64
+	Text  string
 }
 
-var TaxLevels = []TaxLevel{
-	{0, 150000, 0}, // exempt
-	{150000, 500000, 0.10},
-	{500000, 1000000, 0.15},
-	{1000000, 2000000, 0.20},
-	{2000000, math.MaxFloat64, 0.35},
+var TaxCalculatorLevels = []TaxCalculatorLevel{
+	{0, 150000, 0, "0-150,000"}, // exempt
+	{150000, 500000, 0.10, "150,001-500,000"},
+	{500000, 1000000, 0.15, "500,001-1,000,000"},
+	{1000000, 2000000, 0.20, "1,000,001-2,000,000"},
+	{2000000, math.MaxFloat64, 0.35, "2,000,001 ขึ้นไป"},
 }
 
 type TaxService interface {
