aea18428556b3c1dea9ba39f3c355efba7a4c3c1
wariya chanbuala <wariya.wr19@hotmail.com>
2024-04-28 22:07:52 +0700

    add admin handler and service

---
+++ pkg/handlers/admin_test.go
@@ -0,0 +1,103 @@
+package handlers_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/wariusagi/assessment-tax/pkg/handlers"
+	"github.com/wariusagi/assessment-tax/pkg/services"
+)
+
+type MockAdminService struct {
+	res services.AdminDeductionPersonalResponse
+	err error
+}
+
+func (m *MockAdminService) SetDeduction(req services.AdminDeductionRequest) (services.AdminDeductionPersonalResponse, error) {
+	return m.res, m.err
+}
+
+func callAdminHandler(body io.Reader, mockService *MockAdminService) (*httptest.ResponseRecorder, error) {
+	// req
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/tax/calculations", body)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// handler
+	handler := handlers.NewAdminHandler(mockService)
+	err := handler.SetDeduction(c)
+	return rec, err
+}
+
+func TestAdminHandlerSetDeduction_Success(t *testing.T) {
+	reqBody := services.AdminDeductionRequest{}
+	reqBodyJson, err := json.Marshal(reqBody)
+	if err != nil {
+		t.Fatalf("Marshalling request body failed: %v", err)
+	}
+
+	// mock
+	mockService := &MockAdminService{
+		res: services.AdminDeductionPersonalResponse{PersonalDeduction: 70000.0},
+		err: nil,
+	}
+
+	rec, err := callAdminHandler(bytes.NewReader(reqBodyJson), mockService)
+
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, rec.Code)
+
+	resBodyExpected := services.AdminDeductionPersonalResponse{
+		PersonalDeduction: 70000.0,
+	}
+	resBody := services.AdminDeductionPersonalResponse{}
+	if err := json.Unmarshal(rec.Body.Bytes(), &resBody); err != nil {
+		t.Fatalf("Parsing response body failed: %v", err)
+	}
+	assert.Equal(t, resBodyExpected, resBody)
+}
+
+func TestAdminHandlerSetDeduction_ErrorBindReq(t *testing.T) {
+	// req
+	reqBodyStr := `{"amount":"mock"}`
+
+	// mock
+	mockService := &MockAdminService{
+		res: services.AdminDeductionPersonalResponse{},
+		err: nil,
+	}
+
+	rec, err := callAdminHandler(bytes.NewBufferString(reqBodyStr), mockService)
+
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusBadRequest, rec.Code)
+}
+
+func TestAdminHandlerSetDeduction_ErrorSetDeductionService(t *testing.T) {
+	// req
+	reqBody := services.AdminDeductionRequest{Amount: 70000.0}
+	reqBodyJson, err := json.Marshal(reqBody)
+	if err != nil {
+		t.Fatalf("Marshalling request body failed: %v", err)
+	}
+
+	// mock
+	mockService := &MockAdminService{
+		res: services.AdminDeductionPersonalResponse{},
+		err: fmt.Errorf("mock error"),
+	}
+
+	rec, err := callAdminHandler(bytes.NewReader(reqBodyJson), mockService)
+
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusInternalServerError, rec.Code)
+}
---
+++ pkg/services/admin_service_test.go
@@ -0,0 +1,84 @@
+package services_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/wariusagi/assessment-tax/pkg/database"
+	"github.com/wariusagi/assessment-tax/pkg/services"
+)
+
+func (m *MockRepo) UpdateAmtPersonalDeductionDeduction(cycleYear int, amtPersonalDeduction float64) error {
+	return m.err
+}
+
+func callAdminService(req services.AdminDeductionRequest, mockRepo *MockRepo) (services.AdminDeductionPersonalResponse, error) {
+	service := services.NewAdminService(mockRepo)
+	return service.SetDeduction(req)
+
+}
+
+func TestAdminServiceSetDeduction_Success(t *testing.T) {
+	// mock
+	mockRepo := &MockRepo{
+		data: database.MasterTaxDeduction{},
+		err:  nil,
+	}
+	req := services.AdminDeductionRequest{
+		Amount: 70000.0,
+	}
+
+	res, err := callAdminService(req, mockRepo)
+
+	assert.NoError(t, err)
+	resPersonalDeductionExpected := 70000.0
+	assert.Equal(t, resPersonalDeductionExpected, res.PersonalDeduction)
+}
+
+func TestAdminServiceSetDeduction_SuccessWithAmountLimitConfig(t *testing.T) {
+	// mock
+	mockRepo := &MockRepo{
+		data: database.MasterTaxDeduction{},
+		err:  nil,
+	}
+	req := services.AdminDeductionRequest{
+		Amount: 200000.0,
+	}
+
+	res, err := callAdminService(req, mockRepo)
+
+	assert.NoError(t, err)
+	resPersonalDeductionExpected := 100000.0
+	assert.Equal(t, resPersonalDeductionExpected, res.PersonalDeduction)
+}
+
+func TestAdminServiceSetDeduction_ErrorAmountLessThanConfig(t *testing.T) {
+	// mock
+	mockRepo := &MockRepo{
+		data: database.MasterTaxDeduction{},
+		err:  fmt.Errorf("mock error"),
+	}
+	req := services.AdminDeductionRequest{
+		Amount: 5000.0,
+	}
+
+	_, err := callAdminService(req, mockRepo)
+
+	assert.Error(t, err)
+}
+
+func TestAdminServiceSetDeduction_ErrorUpdateDB(t *testing.T) {
+	// mock
+	mockRepo := &MockRepo{
+		data: database.MasterTaxDeduction{},
+		err:  fmt.Errorf("mock error"),
+	}
+	req := services.AdminDeductionRequest{
+		Amount: 70000.0,
+	}
+
+	_, err := callAdminService(req, mockRepo)
+
+	assert.Error(t, err)
+}
